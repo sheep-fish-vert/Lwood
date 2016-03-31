ea6b0d058246c0d34f82ba0e947922874d0115d7
trolizmaslom <trolizmaslom@gmail.com>
2016-03-31 17:27:31 +0300

    wqeqwe

--- src/js/partials/develop_5.js
+++ src/js/partials/develop_5.js
@@ -6,9 +6,48 @@ function orderOpen(){
         }else{$('.order-form-wrap').slideUp('300');}
     });
 }
+function fancyboxTop(){
+  $('.fancybox').fancybox({
+    openEffect  : 'fade',
+    closeEffect : 'fade',
+    autoResize:true,
+    'closeBtn' : false,
+    fitToView:true,
+    padding:'0'
+  })
+}
     $(document).ready(function(){
             orderOpen();
-            headeButer($('.butter'),$('.menu-top'))
+            headeButer($('.butter'),$('.menu-top'));
+            fancyboxTop();
+            $('.slider-top').slick({
+                 dots: true,
+                  infinite: true,
+                  speed: 300,
+                  slidesToShow: 4,
+                  slidesToScroll: 1,
+                  responsive: [
+                    {
+                      breakpoint: 1710,
+                      settings: {
+                        slidesToShow: 3,
+                      }
+                    },
+                    {
+                      breakpoint: 1280,
+                      settings: {
+                        slidesToShow: 2,
+                      }
+                    },
+                    {
+                      breakpoint: 880,
+                      settings: {
+                        slidesToShow: 1,
+                      }
+                    }
+
+                  ]
+            });
     });
 
     $(window).load(function(){
