e9d7a1e8dc19b3c7e6b19128dc827fe85f6911c6
Art Garm <artem.garmatyk@gmail.com>
2016-03-31 16:58:35 +0300

    asafasfd

--- src/js/partials/develop_2.js
+++ src/js/partials/develop_2.js
@@ -9,7 +9,49 @@ try{
         $(".accordion2").accordion({
             heightStyle: "content",
             collapsible: true
-        });       
+        });
+
+        $('.convert-slid').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            arrows: false,
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            //variableWidth: true,
+            responsive: [
+                {
+                    breakpoint: 1400,
+                    settings: {
+                        slidesToShow: 4,
+                   }
+                },
+                {
+                    breakpoint: 1280,
+                    settings: {
+                        slidesToShow: 3,
+                   }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        });        
 
     });
 
